996e7a87ede4d2ba4614faaacdc09827a3a17b5d
Lena Vu <mywayrabota2@gmail.com>
2021-11-17 19:53:21 +0200

    added mock data

--- src/pages/helpers/get-posts.ts
+++ src/pages/helpers/get-posts.ts
@@ -1,6 +1,8 @@
 import { OPTIONS, TIKTOK_FEED_URL } from '../../constants'
+import { trends } from '../../data/data'
 
 export function getVideos() {
     return fetch(TIKTOK_FEED_URL ,OPTIONS)
         .then(response => response.json())
+    //return Promise.resolve(trends)
 }
--- src/pages/helpers/get-user-profile.ts
+++ src/pages/helpers/get-user-profile.ts
@@ -1,7 +1,8 @@
 import { OPTIONS, TIKTOK_URL } from '../../constants'
+import { profile } from '../../data/profile-data'
 
 export function getProfileInfo(profileName: string) {
     return fetch(TIKTOK_URL + profileName , OPTIONS)
         .then(response => response.json())
-
+    //return Promise.resolve(profile)
 }
